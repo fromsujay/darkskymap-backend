a9037f81b7a71a75f00cf90cce1af692ce762753
Florian Merle <merle.florian.84@gmail.com>
2018-11-16 09:43:12 +0100

    heroku error fix attempt

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+let server = require('http').Server(app);
+
 var app = express();
+
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", '*');
   res.header("Access-Control-Allow-Credentials", true);
@@ -15,6 +18,13 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
   next();
 });
+
+const port = process.env.PORT || 8000;
+
+server.listen(port, () => {
+    console.log("App is running on port " + port);
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
